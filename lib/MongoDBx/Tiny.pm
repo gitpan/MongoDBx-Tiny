d24f6d66fbf2b79dc3e002ebdba750017d856a4a
Naoto Ishikawa <toona@toona.org>
2013-01-16 08:33:27 +0000

    Import of TOONA/MongoDBx-Tiny-0.02_02 from CPAN.
    gitpan-cpan-distribution: MongoDBx-Tiny
    gitpan-cpan-version:      0.02_02
    gitpan-cpan-path:         TOONA/MongoDBx-Tiny-0.02_02.tar.gz
    gitpan-cpan-author:       TOONA
    gitpan-cpan-maturity:     developer

--- lib/MongoDBx/Tiny.pm
+++ lib/MongoDBx/Tiny.pm
@@ -10,11 +10,11 @@ MongoDBx::Tiny - Simple Mongo ORM for Perl
 
 =head1 VERSION
 
-Version 0.02_01
+Version 0.02_02
 
 =cut
 
-our $VERSION = '0.02_01';
+our $VERSION = '0.02_02';
 
 =head1 SYNOPSIS
 
@@ -134,7 +134,13 @@ sub new {
 	$class->install_database_name($opt->{database_name});
     }
     my $self = bless{},$class;
-    $self->connect;
+    eval { $self->connect; };
+
+    if ($@) {
+	Carp::carp $@;
+	return;
+    }
+
     return $self;
 }
 
